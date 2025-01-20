7622a43bf522a1a7769d3c029c1ff8ac56fea0ca
Hadryan Silva <hadryan2001@gmail.com>
2025-01-20 15:56:27 -0300

    Initial Commit

---
+++ src/main/java/br/com/hadryan/api/YtDownloadApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.hadryan.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtDownloadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtDownloadApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hadryan/api/YtDownloadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hadryan.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtDownloadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
